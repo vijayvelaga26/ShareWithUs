03c92a181c4b279dce1d3931cc1ea201c152448e
Vijay Velaga <vijay.velaga@gmail.com>
2018-01-13 18:33:35 -0500

    added new route and local Mongo db

--- app.js
+++ app.js
@@ -1,40 +1,64 @@
 var express = require("express");
-var app=express();
-var bodyParser=require("body-parser");
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
-var workshops=[
-    {name: "Robotics workshop", image: "http://www.robosoftsystems.co.in/img/workshop/school_homepage.gif"},
-    {name: "Embeded Workshop", image: "http://turnupworld.com/admin/image/22May2016020530embedx.gif"},
-    {name: "entrepreneur workshop", image: "http://4.bp.blogspot.com/-GgW3juzzUfs/UQhqOUOP86I/AAAAAAABA8E/Kxvb9nsz6QU/s1600/Screenshot+2013-01-29+at+6.22.23+PM.png"},
-    {name: "Robotics workshop", image: "http://www.robosoftsystems.co.in/img/workshop/school_homepage.gif"},
-    {name: "Embeded Workshop", image: "http://turnupworld.com/admin/image/22May2016020530embedx.gif"},
-    {name: "entrepreneur workshop", image: "http://4.bp.blogspot.com/-GgW3juzzUfs/UQhqOUOP86I/AAAAAAABA8E/Kxvb9nsz6QU/s1600/Screenshot+2013-01-29+at+6.22.23+PM.png"}      
-]
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/share_app", { useMongoClient: true })
+//schema setup 
+var workshopSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    host: String,
+    mobile: Number,
+    description: String
+})
 
-  app.get("/",function(req,res){
-      res.render("home");
-  })
+var Workshop = mongoose.model("Workshop", workshopSchema);
+app.get("/", function (req, res) {
+    res.render("home");
+})
 
-  app.get("/workshops",function(req,res){
-    res.render("workshops",{workshops:workshops});
-  })
+app.get("/workshops", function (req, res) {
+    Workshop.find({}, function (err, allWorkshops) {
+        if (err) { console.log(error) }
+        else {
+            res.render("index", { workshops: allWorkshops });
+        }
+    })
+})
+app.post("/workshops", function (req, res) {
+    //get data from forms and add to workshop array
+    var name = req.body.name;
+    var image = req.body.image;
+    var mobile = req.body.mobile;
+    var host = req.body.host;
+    var description = req.body.description
+    var newWorkshop = { name: name, image: image, host: host, mobile: mobile, description: description }
+    Workshop.create(newWorkshop, function (err, data) {
+        if (err) { console.log(error) }
+        else {
+            console.log("newly added workshop :" + data)
+            res.redirect("/workshops");
+        }
+    });
+})
 
-  app.post("/workshops",function(req,res){
-      //get data from forms and add to workshop array
-      var name = req.body.name;
-      var image=req.body.image;
-      var newWorkshop = {name:name, image:image}
-      workshops.push(newWorkshop);
-     // redirect to Workshops page 
-      res.redirect("/workshops");
-  })
+app.get("/workshops/new", function (req, res) {
+    res.render("new");
+})
 
-  app.get("/workshops/new",function(req,res){
-      res.render("new");
-  })
-  
-  app.listen(8000,function(){
-      console.log("server running on port 8000");
+app.get("/workshops/:id", function (req, res) {
+    Workshop.findById(req.params.id, function (err, workshopinfo) {
+        if (err) { console.log(err) }
+        else {
+            res.render("show", { workshop: workshopinfo });
+        }
+    })
 
-  })
+})
+
+app.listen(8000, function () {
+    console.log("server running on port 8000");
+})
+
